c56d11ca560b77b0346493aaa5af4d1565cb6c8a
kacper <kacper.smaga@microsoft.wsei.edu.pl>
2025-03-05 01:19:54 +0100

    feat: add Private method to BlazorChatSampleHub

--- BlazorChat/Components/Hubs/BlazorChatHub.cs
+++ BlazorChat/Components/Hubs/BlazorChatHub.cs
@@ -22,4 +22,9 @@ public class BlazorChatHub : Hub
         Console.WriteLine($"Disconnected {e?.Message} {Context.ConnectionId}");
         await base.OnDisconnectedAsync(e);
     }
+    
+    public async Task Private(string from, string to, string message)
+    {
+        await Clients.Client(to).SendAsync("Private", from, message);
+    }
 }
